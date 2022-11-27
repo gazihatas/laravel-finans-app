dd2189c4fd3c64bda5e18b9344dc33e290c439fe
Gazi Hataş <gazihatas71@gmail.com>
2022-11-28 02:01:15 +0300

    Model ve migrations oluşturuldu.

---
+++ database/migrations/2022_11_27_221000_create_musterilers_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('musterilers', function (Blueprint $table) {
+            $table->id();
+            $table->integer('musteriTipi')->default(0);
+            $table->string('photo')->nullable();
+
+            $table->string('ad')->nullable();
+            $table->string('soyad')->nullable();
+            $table->date('dogumTarih')->nullable();
+            $table->string('tc')->nullable();
+
+            $table->string('firmaAdi')->nullable();
+            $table->string('vergiNumarasi')->nullable();
+            $table->string('vergiDairesi')->nullable();
+
+            $table->string('adres')->nullable();
+            $table->string('telefon')->nullable();
+            $table->string('email')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('musterilers');
+    }
+};
